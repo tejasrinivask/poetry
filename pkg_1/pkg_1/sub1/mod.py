8ccb5e97ec7d3cfe019905dd93bea3618ed5366d
tejasrinivask <tejasrinivas@amagi.com>
2024-10-08 17:07:20 +0530

    add poetry packages

---
+++ pkg_1/pkg_1/sub1/mod.py
@@ -0,0 +1,7 @@
+def greet(name: str) -> str:
+    """
+    Function to greet the user.
+    :param name: Name of the user to greet.
+    :return: Greeting message.
+    """
+    return f"Hello, {name}! Welcome to package_one."
