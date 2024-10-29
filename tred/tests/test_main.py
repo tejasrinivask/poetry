e15603a903799a5829511d976bf7bc447d01c645
tejasrinivask <tejasrinivas@amagi.com>
2024-10-29 15:38:24 +0530

    update tred

--- tred/tests/test_main.py
+++ tred/tests/test_main.py
@@ -1,19 +1,40 @@
-from tred.main import Locks, Tred
+from tred import Locks, Tred
+from prometheus_client import Histogram
+
+h = Histogram('request_latency_seconds', 'Description of histogram')
+o = Tred(decode_responses=True, redis_lock_execution_timeout=6, redis_lock_blocking_timeout=6, prometheus_histogram_object=h)
+p = Tred(decode_responses=True, redis_lock_execution_timeout=6, redis_lock_blocking_timeout=6)
 
 def test_execute():
-    r = Tred(decode_responses=True, redis_lock_execution_timeout=6, redis_lock_blocking_timeout=6)
-    r.execute("set", "tj", "k", lock_type=Locks.DISTRIBUTED)
-    r.execute("set", "tj", "mika", lock_type=Locks.DISTRIBUTED)
-    r.execute("hset", "caps", "b", "B", mapping={"c": "C"}, lock_type=Locks.LOCAL)
-    r.execute("hset", "caps", "d", "D", mapping={"e": "E"})
-    r.execute("hset", "caps", "a", "A", lock_type = Locks.LOCAL)
-    assert r.execute("hget", "caps", "e", lock_type=Locks.LOCAL) == "E"
-    assert r.execute("hget", "caps", "f", lock_type=Locks.LOCAL) == None
-    assert r.execute("hget", "caps", "a", lock_type = Locks.DISTRIBUTED) == "A"
-    assert r.execute("hget", "caps", "b", lock_type = Locks.LOCAL) == "B"
-    with r.r.lock(name="own"):
-        r.execute("set", "k", "u")
-        r.execute("set", "z", "a")
-        assert r.execute("get", "z") == "a"
-        assert r.execute("get", "k") == "u"
+    assert o.execute("set", "tj", "k", lock_type=Locks.DISTRIBUTED) == True
+    assert o.execute("set", "tj", "mika", lock_type=Locks.DISTRIBUTED) == True
+    assert o.execute("hset", "caps", "b", "B", mapping={"c": "C"}, lock_type=Locks.LOCAL) == 0
+    assert o.execute("hset", "caps", "d", "D", mapping={"e": "E"}) == 0
+    assert o.execute("hset", "caps", "a", "A", lock_type = Locks.LOCAL) == 0
+    assert o.execute("hget", "caps", "e", lock_type=Locks.LOCAL) == "E"
+    assert o.execute("hget", "caps", "f", lock_type=Locks.LOCAL) == None
+    assert o.execute("hget", "caps", "a", lock_type = Locks.DISTRIBUTED) == "A"
+    assert o.execute("hget", "caps", "b", lock_type = Locks.LOCAL) == "B"
+    assert o.execute("hget", "caps", "b", lock_type = "random string") == None
+    with o.r.lock(name="own"):
+        assert o.execute("set", "k", "u") == True
+        assert o.execute("set", "z", "a") == True
+        assert o.execute("get", "z") == "a"
+        assert o.execute("get", "k") == "u"
+
+    assert p.execute("set", "tj", "k", lock_type=Locks.DISTRIBUTED) == True
+    assert p.execute("set", "tj", "mika", lock_type=Locks.DISTRIBUTED) == True
+    assert p.execute("hset", "caps", "b", "B", mapping={"c": "C"}, lock_type=Locks.LOCAL) == 0
+    assert p.execute("hset", "caps", "d", "D", mapping={"e": "E"}) == 0
+    assert p.execute("hset", "caps", "a", "A", lock_type = Locks.LOCAL) == 0
+    assert p.execute("hget", "caps", "e", lock_type=Locks.LOCAL) == "E"
+    assert p.execute("hget", "caps", "f", lock_type=Locks.LOCAL) == None
+    assert p.execute("hget", "caps", "a", lock_type = Locks.DISTRIBUTED) == "A"
+    assert p.execute("hget", "caps", "b", lock_type = Locks.LOCAL) == "B"
+    assert p.execute("hget", "caps", "b", lock_type = "random string") == None
+    with p.r.lock(name="own"):
+        assert p.execute("set", "k", "u") == True
+        assert p.execute("set", "z", "a") == True
+        assert p.execute("get", "z") == "a"
+        assert p.execute("get", "k") == "u"
 
--- tred/tred/main.py
+++ tred/tred/main.py
@@ -1,9 +1,12 @@
-import time
+import logging
 from enum import Enum, unique
 from redis import Redis
 from threading import Lock, Thread
-from typing import Any, Dict
+from typing import Any, Dict, Union
 from tred.utils.singleton import singleton
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s, %(levelname)s, [%(filename)s:%(lineno)d] Message: %(message)s', datefmt='%Y-%m-%d:%H:%M:%S', level=logging.INFO)
 
 @unique
 class Locks(Enum):
@@ -13,47 +16,61 @@ class Locks(Enum):
 
 @singleton
 class Tred:
-    def __init__(self, *args, redis_lock_execution_timeout: float = .1, redis_lock_blocking_timeout: float = .1, **kwargs) -> Any:
+    def __init__(self, *args, redis_lock_execution_timeout: float = .1, redis_lock_blocking_timeout: float = .1, prometheus_histogram_object: Union[Any, None] = None, **kwargs) -> Any:
         self.r = Redis(*args, **kwargs)
         self.local_locks_map:Dict = {}
         self.redis_lock_execution_timeout: float = redis_lock_execution_timeout
         self.redis_lock_blocking_timeout: float = redis_lock_blocking_timeout
+        self.prometheus_histogram_object: Union[Any, None] = prometheus_histogram_object
 
     def execute(self, f_name: str, *args, lock_type: Locks = Locks.NONE, lock_name: str = "", **kwargs):
         f = getattr(self.r, f_name)
         ret = None
-        if lock_type == Locks.NONE:
-            #print("No lock")
-            ret = f(*args, **kwargs)
-        elif lock_type == Locks.LOCAL:
-            if not lock_name:
-                lock_name = f_name
-            if lock_name not in self.local_locks_map:
-                self.local_locks_map[lock_name] = Lock()
-            with self.local_locks_map[lock_name]:
-                # print(f"acquired local lock for {lock_name}")
-                time.sleep(1)
-                ret = f(*args, **kwargs)
-        elif lock_type == Locks.DISTRIBUTED:
-            if not lock_name:
-                lock_name = f_name
-            with self.r.lock(name=lock_name, timeout=self.redis_lock_execution_timeout, blocking_timeout=self.redis_lock_blocking_timeout):
-                # print(f"acquired distributed lock for {lock_name}")
-                ret = f(*args, **kwargs)
-        else:
-            print("Unknown lock type, supported locks:", (", ".join([f'Locks.{x.name}' for x in Locks])))
+        match lock_type:
+            case Locks.NONE:
+                if self.prometheus_histogram_object:
+                    with self.prometheus_histogram_object.time():
+                        return f(*args, **kwargs)
+                else:
+                    return f(*args, **kwargs)
+            case Locks.LOCAL:
+                if not lock_name:
+                    lock_name = f_name
+                if lock_name not in self.local_locks_map:
+                    self.local_locks_map[lock_name] = Lock()
+                with self.local_locks_map[lock_name]:
+                    # print(f"acquired distributed lock for {lock_name}")
+                    if self.prometheus_histogram_object:
+                        with self.prometheus_histogram_object.time():
+                            return f(*args, **kwargs)
+                    else:
+                        return f(*args, **kwargs)
+            case Locks.DISTRIBUTED:
+                if not lock_name:
+                    lock_name = f_name
+                with self.r.lock(name=lock_name, timeout=self.redis_lock_execution_timeout, blocking_timeout=self.redis_lock_blocking_timeout):
+                    # print(f"acquired distributed lock for {lock_name}")
+                    if self.prometheus_histogram_object:
+                        with self.prometheus_histogram_object.time():
+                            return f(*args, **kwargs)
+                    else:
+                        return f(*args, **kwargs)
+            case _:
+                msg = f"Unknown lock type -> '{lock_type}', supported locks:" + (", ".join([f'Locks.{x.name}' for x in Locks]))
+                logger.error(msg)
+                return None
         return ret
 
-
 def main():
-    r = Tred(decode_responses=True, redis_lock_execution_timeout=6, redis_lock_blocking_timeout=6)
-    print(r.execute("set", "tj", "mika", lock_type=Locks.DISTRIBUTED))
-    print(r.execute("get", "tj", lock_type=Locks.DISTRIBUTED))
-    with r.r.lock(name="own"):
-        r.execute("set", "k", "u")
-        r.execute("set", "z", "a")
-        print(r.execute("get", "z"))
-        print(r.execute("get", "k"))
+    o = Tred(decode_responses=True, redis_lock_execution_timeout=6, redis_lock_blocking_timeout=6)
+    print(o.execute("set", "tj", "mika", lock_type=Locks.DISTRIBUTED))
+    print(o.execute("get", "tj", lock_type=Locks.DISTRIBUTED))
+    print(o.execute("get", "tj", lock_type="Something"))
+    with o.r.lock(name="test"):
+        print(o.execute("set", "k", "u"))
+        print(o.execute("set", "z", "a"))
+        print(o.execute("get", "z"))
+        print(o.execute("get", "k"))
 
 if __name__ == '__main__':
     main()
